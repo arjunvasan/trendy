ea080a9418ade26b22ea7b62c148d8587e1516fa
arjunvasan <arjun.vasan@gmail.com>
2017-01-12 20:03:03 -0800

    push

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2016 Arjun Vasan <arjun.vasan@gmail.com>
 
 from __future__ import division
-
+from webapp2_extras import jinja2
 import webapp2
-
 import os
 import json
 import base64
 import urllib
 from google.appengine.ext.webapp import template
 from google.appengine.api import urlfetch
-
 from datetime import datetime, timedelta
-
+from itertools import islice, tee
 
 intrinio_un = "f14a7f21a25d12f05be67615ac078841"
 intrinio_pw = "2c75726a36c24361e8aeb00b769904d1"
 
+gProps = {
+    "":"Web Search",
+    "news":"Google News",
+    "youtube":"Youtube",
+    "images":"Google Images",
+    "froogle":"Google Shopping"
+}
 
+gDates = {
+    "":"2004 > Today",
+    "today 60-m":"Past 5 Years",
+    "today 12-m":"Past Year",
+    "today 90-d":"Past 90 Days",
+    "today 30-d":"Past 30 Days",
+    "today 7-d":"Past 7 Days"
+}
 
-from itertools import islice, tee
+class BaseHandler(webapp2.RequestHandler):
+
+    @webapp2.cached_property
+    def jinja2(self):
+        # Returns a Jinja2 renderer cached in the app registry.
+        return jinja2.get_jinja2(app=self.app)
+
+    def render_response(self, _template, **context):
+        # Renders a template and writes the result to the response.
+        rv = self.jinja2.render_template(_template, **context)
+        self.response.write(rv)
+
 
 def moving_average(n, iterable):
     # leading 0s
@@ -50,17 +62,11 @@ class BtcData(webapp2.RequestHandler):
     def get(self):
         request_url = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-22&end=2017-12-30"
         result = urlfetch.fetch(request_url)
-
-        result = urlfetch.fetch(request_url)
         ticker = json.loads(result.content)
 
         csv = ["%s,%s" % (k,ticker["bpi"][k]) for k in sorted(ticker["bpi"])][::7]
-
         csv_text = "<br>".join(csv)
-
         ticker = [ticker["bpi"][k] for k in sorted(ticker["bpi"])][::7]
-
-
 
         self.response.out.write(csv_text)
 
@@ -72,12 +78,9 @@ class AddTrend(webapp2.RequestHandler):
         geo = self.request.get("geo")
         prop = self.request.get("prop")
         request_url = 'https://www.google.com/trends/fetchComponent?q=%s&cid=TIMESERIES_GRAPH_0&export=3&date=%s&geo=%s&gprop=%s' % (q,date,geo,prop)
-
         result = urlfetch.fetch(request_url)
 
         self.response.out.write(result.content)
-
-
 
 
 class AddStock(webapp2.RequestHandler):
@@ -90,7 +93,6 @@ class AddStock(webapp2.RequestHandler):
                 result = urlfetch.fetch(request_url)
                 ticker = json.loads(result.content)
                 ticker = [ticker["bpi"][k] for k in sorted(ticker["bpi"])][::30]
-
 
                 ticker_x = ticker
                 ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
@@ -110,17 +112,11 @@ class AddStock(webapp2.RequestHandler):
                 ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
                 ticker = [round(100*x,4)for x in ticker]
 
-
-                #print ticker
-
             elif stock.lower() == "gold":
                 request_url = "https://www.quandl.com/api/v3/datasets/COM/AU_LAM.json?api_key=Q6YzQTqA1mSKaaYL-Cb3&frequency=monthly"
                 result = urlfetch.fetch(request_url)
                 ticker = json.loads(result.content)
                 print ticker["dataset"]["data"]
-
-
-
 
             else:
                 request_url = "https://api.intrinio.com/prices?identifier=%s&item=close&start_date=2004-01-01&frequency=monthly" % stock.upper()
@@ -132,15 +128,10 @@ class AddStock(webapp2.RequestHandler):
                                                  "Basic %s" % base64.b64encode("f14a7f21a25d12f05be67615ac078841:2c75726a36c24361e8aeb00b769904d1")})
 
                 ticker = json.loads(result.content)
-
                 ticker = [x["close"] for x in ticker["data"]]
-
                 ticker.reverse()
-
                 ticker_x = ticker
-
                 ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
-
                 ticker = [round(100*x,4)for x in ticker]
         else:
             ticker = []
@@ -156,7 +147,6 @@ def getTicker(symbol):
             ticker = json.loads(result.content)
             ticker = [ticker["bpi"][k] for k in sorted(ticker["bpi"])][::30]
 
-
             ticker_x = ticker
             ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
             ticker = [round(100*x,4)for x in ticker]
@@ -171,10 +161,7 @@ def getTicker(symbol):
             ticker = json.loads(result.content)["dataset"]["data"][:157]
 
             ticker.reverse()
-
             ticker = [t[1] for t in ticker]
-
-            print ticker
 
             ticker_x = ticker
             ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
@@ -189,7 +176,6 @@ def getTicker(symbol):
             ticker_x = ticker
             ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
             ticker = [round(100*x,4)for x in ticker]
-
 
         else:
             request_url = "https://api.intrinio.com/prices?identifier=%s&item=close&start_date=2004-01-01&frequency=monthly" % stock.upper()
@@ -213,7 +199,7 @@ def getTicker(symbol):
     return ticker,ticker_x
 
 
-class MainPage(webapp2.RequestHandler):
+class MainPage(BaseHandler):
     def get(self):
 
         trend_line = self.request.get('trend_line')
@@ -253,14 +239,10 @@ class MainPage(webapp2.RequestHandler):
         if self.request.get("old"):
             old = True
 
-
         keywords = trend_line.split(",")
-
-
 
         ticker_time = []
         ticker_dict = {}
-
 
         if stock:
 
@@ -279,24 +261,11 @@ class MainPage(webapp2.RequestHandler):
                     request_url = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-17&end=2017-12-30"
                     result = urlfetch.fetch(request_url)
                     ticker = json.loads(result.content)
-                    #ticker_time = [k for k in sorted(ticker["bpi"])][::frequency]
-                    #ticker_time = json.dumps(ticker_time);
-                    #print sorted(ticker["bpi"])
-                    #print "hello"
 
-
-                    #ticker_time = ticker_time.replace("&quot;",'"')
                     combined_ticker = [[k,ticker["bpi"][k]] for k in sorted(ticker["bpi"])][::frequency]
 
                     ticker = [ct[1] for ct in combined_ticker]
                     ticker_time = [ct[0] for ct in combined_ticker]
-                    print "hellotime"
-                    print ticker_time
-                    
-                    
-
-                    #ticker = list(moving_average(3,ticker))
-
 
                 ticker_x = ticker
                 ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
@@ -312,10 +281,7 @@ class MainPage(webapp2.RequestHandler):
                 ticker = json.loads(result.content)["dataset"]["data"][:157]
 
                 ticker.reverse()
-
                 ticker = [t[1] for t in ticker]
-
-
 
                 ticker_x = ticker
                 ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
@@ -331,9 +297,7 @@ class MainPage(webapp2.RequestHandler):
                 ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
                 ticker = [round(100*x,4)for x in ticker]
 
-
             else:
-
                 if hourly:
                     request_url = "http://chartapi.finance.yahoo.com/instrument/1.0/%s/chartdata;type=quote;range=10d/json" % stock
                     result = urlfetch.fetch(request_url)
@@ -351,8 +315,6 @@ class MainPage(webapp2.RequestHandler):
                             ticker_dict[datetime.fromtimestamp(x["Timestamp"]).strftime("%Y-%m-%d %H")] = x["close"]
 
 
-                    #print ticker_dict
-
                     base = datetime.today()
                     date_list = [(base - timedelta(hours=x)).strftime("%Y-%m-%d %H") for x in range(0, 240)]
                     
@@ -367,11 +329,8 @@ class MainPage(webapp2.RequestHandler):
                                 ticker_full.append(ticker_full[-1])
 
                     ticker = ticker_full
-
                     ticker_x = ticker
-                    
                     ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
-
                     ticker = [round(100*x,4)for x in ticker]
 
 
@@ -393,17 +352,12 @@ class MainPage(webapp2.RequestHandler):
                                                      "Basic %s" % base64.b64encode("f14a7f21a25d12f05be67615ac078841:2c75726a36c24361e8aeb00b769904d1")})
 
                     ticker = json.loads(result.content)
-
                     new_ticker = []
-
 
                     ticker_dict = {}
                     for x in ticker["data"]:
-
                         new_ticker.append(x["close"])
-
                         ticker_dict[x["date"]] = x["close"]
-
 
                     base = datetime.today()
                     if f == "daily":
@@ -415,44 +369,25 @@ class MainPage(webapp2.RequestHandler):
                             if d in ticker_dict:
                                 started = True
                                 ticker_full.append(ticker_dict[d])
-                                print d, ticker_dict[d]
                             else:
                                 if started:
                                     ticker_dict[d] = ticker_full[-1]
-                                    print d, ticker_full[-1]
                                     ticker_full.append(ticker_full[-1])
                         
-
                         ticker = ticker_full
-                        #print ticker_full,len(ticker_full)
+
                     else:
                         ticker = new_ticker
 
-
-                    
                     ticker.reverse()
-                    #ticker_date.reverse()
-
-
-
-                    #d = [datetime.strptime(d,"%Y-%m-%d") for d in ticker_date]
-
-                    #date_set = set(d[0] + timedelta(x) for x in range((d[-1] - d[0]).days))
-                    #missing = sorted(date_set - set(d))
-
-
-                    #print ticker_date
-
                     ticker_x = ticker
                     ticker = [(x-min(ticker))/(max(ticker)-min(ticker)) for x in ticker]
                     ticker = [round(100*x,4)for x in ticker]
-
 
         else:
             ticker = []
             ticker_x = []
 
-        
         template_values = {
             'trend_line': urllib.quote(trend_line),
             'trend_clean':trend_line,
@@ -470,14 +405,16 @@ class MainPage(webapp2.RequestHandler):
             'ticker':ticker,
             'ticker_dict':ticker_dict,
             'gprop':self.request.get("gprop"),
+            'gprop_text':gProps[self.request.get("gprop")],
+            'gdates_text':gDates[self.request.get("date")],
             'ticker_x':ticker_x,
             'url':self.request.url,
             'ticker_time':ticker_time,
             'step':self.request.get("step")}
 
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
+        self.render_response('index.html', **template_values)
+
 
 
 
